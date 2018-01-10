af9326d3b865bb52a03d1a1050a9aad4bc04f8bd
BFunk <bret.e.funk@gmail.com>
2018-01-10 15:28:13 -0500

    first commit

---
+++ flatten.rb
@@ -0,0 +1,16 @@
+require 'byebug'
+
+def flatten(array, results=[])
+  array.each do |element|
+    if element.class == Array
+      flatten(element, results)
+    else
+      results << element
+    end
+  end
+  results
+end
+
+print flatten([[1,2,[3]],4])
+print flatten([[[1],2,[3]],4])
+print flatten([[1,[2,[3]]],4])
