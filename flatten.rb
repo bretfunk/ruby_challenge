75765aba311f8b8cc7a9e8ec1559ba9179fe478d
Bret Funk <bret.e.funk@gmail.com>
2018-01-10 15:46:00 -0500

    Update flatten.rb

--- flatten.rb
+++ flatten.rb
@@ -1,13 +1,14 @@
-class Flatten
-  def flatten_array(array, results=[])
-    array.each do |element|
-      if element.class == Array
-        flatten_array(element, results)
-      else
-        results << element
-      end
-    end
-    results
-  end
-end
-
+   class Flatten
+     
+   def flatten_array(array, results=[])
+     array.each do |element|
+       if element.class == Array
+         flatten_array(element, results)
+       else
+         results << element
+       end
+     end
+     results
+   end
+     
+ end
